742ab57957a4cd6af58ac32365da3ff5ee3f2481
jefferyWU0617 <145523960+jefferywu0109@users.noreply.github.com>
2023-09-20 18:20:55 +0800

    1

---
+++ disableBTN.js
@@ -0,0 +1,206 @@
+	
+	function  disableBtn()
+	{
+		document.getElementById("Btn").disabled = true;	
+	}
+
+	function  disableBtn1()
+	{
+		document.getElementById("Btn1").disabled = true;	
+	}
+
+	function  disableBtn2()
+	{
+		document.getElementById("Btn2").disabled = true;	
+	}
+	
+	function  disableBtn3()
+	{
+		document.getElementById("Btn3").disabled = true;	
+	}
+	
+	function  disableBtn4()
+	{
+		document.getElementById("Btn4").disabled = true;	
+	}
+
+	function  disableBtn5()
+	{
+		document.getElementById("Btn5").disabled = true;	
+	}
+
+	function  disableBtn6()
+	{
+		document.getElementById("Btn6").disabled = true;	
+	}
+
+	function  disableBtn7()
+	{
+		document.getElementById("Btn7").disabled = true;	
+	}
+
+	function  disableBtn8()
+	{
+		document.getElementById("Btn8").disabled = true;	
+	}
+
+	function  disableBtn9()
+	{
+		document.getElementById("Btn9").disabled = true;	
+	}
+
+	function  disableBtn10()
+	{
+		document.getElementById("Btn10").disabled = true;	
+	}
+
+	function  disableBtn11()
+	{
+		document.getElementById("Btn11").disabled = true;	
+	}
+
+	function  disableBtn12()
+	{
+		document.getElementById("Btn12").disabled = true;	
+	}
+
+	function  disableBtn13()
+	{
+		document.getElementById("Btn13").disabled = true;	
+	}
+
+	function  disableBtn14()
+	{
+		document.getElementById("Btn14").disabled = true;	
+	}
+
+	function  disableBtn15()
+	{
+		document.getElementById("Btn15").disabled = true;	
+	}
+
+	function  disableBtn16()
+	{
+		document.getElementById("Btn16").disabled = true;	
+	}
+
+	function  disableBtn17()
+	{
+		document.getElementById("Btn17").disabled = true;	
+	}
+
+	function  disableBtn18()
+	{
+		document.getElementById("Btn18").disabled = true;	
+	}
+
+	function  disableBtn19()
+	{
+		document.getElementById("Btn19").disabled = true;	
+	}
+
+	function  disableBtn20()
+	{
+		document.getElementById("Btn20").disabled = true;	
+	}
+
+	function  disableBtn21()
+	{
+		document.getElementById("Btn21").disabled = true;	
+	}
+
+	function  disableBtn22()
+	{
+		document.getElementById("Btn22").disabled = true;	
+	}
+
+	function  disableBtn23()
+	{
+		document.getElementById("Btn23").disabled = true;	
+	}
+
+	function  disableBtn24()
+	{
+		document.getElementById("Btn24").disabled = true;	
+	}
+
+	function  disableBtn25()
+	{
+		document.getElementById("Btn25").disabled = true;	
+	}
+
+	function  disableBtn26()
+	{
+		document.getElementById("Btn26").disabled = true;	
+	}
+
+	function  disableBtn27()
+	{
+		document.getElementById("Btn27").disabled = true;	
+	}
+
+	function  disableBtn28()
+	{
+		document.getElementById("Btn28").disabled = true;	
+	}
+
+	function  disableBtn29()
+	{
+		document.getElementById("Btn29").disabled = true;	
+	}
+
+	function  disableBtn30()
+	{
+		document.getElementById("Btn30").disabled = true;	
+	}
+
+	function  disableBtn31()
+	{
+		document.getElementById("Btn31").disabled = true;	
+	}
+
+	function  disableBtn32()
+	{
+		document.getElementById("Btn32").disabled = true;	
+	}
+
+	function  disableBtn33()
+	{
+		document.getElementById("Btn33").disabled = true;	
+	}
+
+	function  disableBtn34()
+	{
+		document.getElementById("Btn34").disabled = true;	
+	}
+
+	function  disableBtn35()
+	{
+		document.getElementById("Btn35").disabled = true;	
+	}
+
+	function  disableBtn36()
+	{
+		document.getElementById("Btn36").disabled = true;	
+	}
+
+	function  disableBtn37()
+	{
+		document.getElementById("Btn37").disabled = true;	
+	}
+
+	function  disableBtn38()
+	{
+		document.getElementById("Btn38").disabled = true;	
+	}
+
+	function  disableBtn39()
+	{
+		document.getElementById("Btn39").disabled = true;	
+	}
+
+	function  disableBtn40()
+	{
+		document.getElementById("Btn40").disabled = true;	
+	}
+
