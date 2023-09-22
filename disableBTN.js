f896a8cd8cff0299e55dcf61eb200d88067d5c56
jefferyWU0617 <145523960+jefferywu0109@users.noreply.github.com>
2023-09-22 18:49:25 +0800

    Update disableBTN.js

--- disableBTN.js
+++ disableBTN.js
@@ -5,14 +5,16 @@
 		function  disableBtn4()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40";
-			document.getElementById("Btn4").disabled = true;	
+			document.getElementById("Btn4").disabled = true;
+			document.getElementById("pic4").src = "2.jpg";	
 		}
 	document.getElementById("Btn").onclick = function() {disableBtn()};
 		
 		function  disableBtn()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn").disabled = true;	
+			document.getElementById("Btn").disabled = true;
+      			document.getElementById("pic").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn1").onclick = function() {disableBtn1()};
@@ -20,7 +22,8 @@
 		function  disableBtn1()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn1").disabled = true;	
+			document.getElementById("Btn1").disabled = true;
+			document.getElementById("pic1").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn2").onclick = function() {disableBtn2()};
@@ -28,7 +31,8 @@
 		function  disableBtn2()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn2").disabled = true;	
+			document.getElementById("Btn2").disabled = true;
+			document.getElementById("pic2").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn3").onclick = function() {disableBtn3()};
@@ -36,7 +40,8 @@
 		function  disableBtn3()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn3").disabled = true;	
+			document.getElementById("Btn3").disabled = true;
+			document.getElementById("pic3").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn5").onclick = function() {disableBtn5()};
@@ -44,7 +49,8 @@
 		function  disableBtn5()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn5").disabled = true;	
+			document.getElementById("Btn5").disabled = true;
+			document.getElementById("pic5").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn6").onclick = function() {disableBtn6()};
@@ -52,7 +58,8 @@
 		function  disableBtn6()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn6").disabled = true;	
+			document.getElementById("Btn6").disabled = true;
+			document.getElementById("pic6").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn7").onclick = function() {disableBtn7()};
@@ -60,7 +67,8 @@
 		function  disableBtn7()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn7").disabled = true;	
+			document.getElementById("Btn7").disabled = true;
+			document.getElementById("pic7").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn8").onclick = function() {disableBtn8()};
@@ -68,7 +76,8 @@
 		function  disableBtn8()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn8").disabled = true;	
+			document.getElementById("Btn8").disabled = true;
+			document.getElementById("pic8").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn9").onclick = function() {disableBtn9()};
@@ -76,7 +85,8 @@
 		function  disableBtn9()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn9").disabled = true;	
+			document.getElementById("Btn9").disabled = true;
+			document.getElementById("pic9").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn10").onclick = function() {disableBtn10()};
@@ -84,7 +94,8 @@
 		function  disableBtn10()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn10").disabled = true;	
+			document.getElementById("Btn10").disabled = true;
+			document.getElementById("pic10").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn11").onclick = function() {disableBtn11()};
@@ -92,7 +103,8 @@
 		function  disableBtn11()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn11").disabled = true;	
+			document.getElementById("Btn11").disabled = true;
+			document.getElementById("pic11").src = "2.jpg";	
 		}
 	
 	document.getElementById("Btn12").onclick = function() {disableBtn12()};
@@ -100,7 +112,8 @@
 		function  disableBtn12()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn12").disabled = true;	
+			document.getElementById("Btn12").disabled = true;
+			document.getElementById("pic12").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn13").onclick = function() {disableBtn13()};
@@ -108,7 +121,8 @@
 		function  disableBtn13()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn13").disabled = true;	
+			document.getElementById("Btn13").disabled = true;
+			document.getElementById("pic13").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn14").onclick = function() {disableBtn14()};
@@ -116,7 +130,8 @@
 		function  disableBtn14()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn14").disabled = true;	
+			document.getElementById("Btn14").disabled = true;
+			document.getElementById("pic14").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn15").onclick = function() {disableBtn15()};
@@ -124,7 +139,8 @@
 		function  disableBtn15()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn15").disabled = true;	
+			document.getElementById("Btn15").disabled = true;
+			document.getElementById("pic15").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn16").onclick = function() {disableBtn16()};
@@ -132,7 +148,8 @@
 		function  disableBtn16()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn16").disabled = true;	
+			document.getElementById("Btn16").disabled = true;
+			document.getElementById("pic16").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn17").onclick = function() {disableBtn17()};
@@ -140,7 +157,8 @@
 		function  disableBtn17()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn17").disabled = true;	
+			document.getElementById("Btn17").disabled = true;
+			document.getElementById("pic17").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn18").onclick = function() {disableBtn18()};
@@ -148,7 +166,8 @@
 		function  disableBtn18()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn18").disabled = true;	
+			document.getElementById("Btn18").disabled = true;
+			document.getElementById("pic18").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn19").onclick = function() {disableBtn19()};
@@ -156,7 +175,8 @@
 		function  disableBtn19()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn19").disabled = true;	
+			document.getElementById("Btn19").disabled = true;
+			document.getElementById("pic19").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn20").onclick = function() {disableBtn20()};
@@ -164,7 +184,8 @@
 		function  disableBtn20()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn20").disabled = true;	
+			document.getElementById("Btn20").disabled = true;
+			document.getElementById("pic20").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn21").onclick = function() {disableBtn21()};
@@ -172,7 +193,8 @@
 		function  disableBtn21()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn21").disabled = true;	
+			document.getElementById("Btn21").disabled = true;
+			document.getElementById("pic21").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn22").onclick = function() {disableBtn22()};
@@ -180,7 +202,8 @@
 		function  disableBtn22()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn22").disabled = true;	
+			document.getElementById("Btn22").disabled = true;
+			document.getElementById("pic22").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn23").onclick = function() {disableBtn23()};
@@ -188,7 +211,8 @@
 		function  disableBtn23()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn23").disabled = true;	
+			document.getElementById("Btn23").disabled = true;
+			document.getElementById("pic23").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn24").onclick = function() {disableBtn24()};
@@ -196,7 +220,8 @@
 		function  disableBtn24()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn24").disabled = true;	
+			document.getElementById("Btn24").disabled = true;
+			document.getElementById("pic24").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn25").onclick = function() {disableBtn25()};
@@ -204,7 +229,8 @@
 		function  disableBtn25()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn25").disabled = true;	
+			document.getElementById("Btn25").disabled = true;
+			document.getElementById("pic25").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn26").onclick = function() {disableBtn26()};
@@ -212,7 +238,8 @@
 		function  disableBtn26()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn26").disabled = true;	
+			document.getElementById("Btn26").disabled = true;
+			document.getElementById("pic26").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn27").onclick = function() {disableBtn27()};
@@ -220,7 +247,8 @@
 		function  disableBtn27()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn27").disabled = true;	
+			document.getElementById("Btn27").disabled = true;
+			document.getElementById("pic27").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn28").onclick = function() {disableBtn28()};
@@ -228,7 +256,8 @@
 		function  disableBtn28()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn28").disabled = true;	
+			document.getElementById("Btn28").disabled = true;
+			document.getElementById("pic28").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn29").onclick = function() {disableBtn29()};
@@ -236,7 +265,8 @@
 		function  disableBtn29()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn29").disabled = true;	
+			document.getElementById("Btn29").disabled = true;
+			document.getElementById("pic29").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn30").onclick = function() {disableBtn30()};
@@ -244,7 +274,8 @@
 		function  disableBtn30()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn30").disabled = true;	
+			document.getElementById("Btn30").disabled = true;
+			document.getElementById("pic30").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn31").onclick = function() {disableBtn31()};
@@ -252,7 +283,8 @@
 		function  disableBtn31()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn31").disabled = true;	
+			document.getElementById("Btn31").disabled = true;
+			document.getElementById("pic31").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn32").onclick = function() {disableBtn32()};
@@ -260,7 +292,8 @@
 		function  disableBtn32()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn32").disabled = true;	
+			document.getElementById("Btn32").disabled = true;
+			document.getElementById("pic32").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn33").onclick = function() {disableBtn33()};
@@ -268,7 +301,8 @@
 		function  disableBtn33()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn33").disabled = true;	
+			document.getElementById("Btn33").disabled = true;
+			document.getElementById("pic33").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn34").onclick = function() {disableBtn34()};
@@ -276,7 +310,8 @@
 		function  disableBtn34()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn34").disabled = true;	
+			document.getElementById("Btn34").disabled = true;
+			document.getElementById("pic34").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn35").onclick = function() {disableBtn35()};
@@ -284,7 +319,8 @@
 		function  disableBtn35()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn35").disabled = true;	
+			document.getElementById("Btn35").disabled = true;
+			document.getElementById("pic35").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn36").onclick = function() {disableBtn36()};
@@ -292,7 +328,8 @@
 		function  disableBtn36()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn36").disabled = true;	
+			document.getElementById("Btn36").disabled = true;
+			document.getElementById("pic36").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn37").onclick = function() {disableBtn37()};
@@ -300,7 +337,8 @@
 		function  disableBtn37()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn37").disabled = true;	
+			document.getElementById("Btn37").disabled = true;
+			document.getElementById("pic37").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn38").onclick = function() {disableBtn38()};
@@ -308,7 +346,8 @@
 		function  disableBtn38()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn38").disabled = true;	
+			document.getElementById("Btn38").disabled = true;
+			document.getElementById("pic38").src = "2.jpg";	
 		}
 
 	document.getElementById("Btn39").onclick = function() {disableBtn39()};
@@ -316,7 +355,8 @@
 		function  disableBtn39()
 		{	
 			document.getElementById("num").innerHTML = "剩餘格數 ："+  --x +" " + "/ 40" ;
-			document.getElementById("Btn39").disabled = true;	
+			document.getElementById("Btn39").disabled = true;
+			document.getElementById("pic39").src = "2.jpg";	
 		}
 
 
