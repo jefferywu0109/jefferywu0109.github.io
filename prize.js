fe3fa778c5e5d8ed60e5c8ea9310249d5e3cbea6
jefferyWU0617 <145523960+jefferywu0109@users.noreply.github.com>
2023-09-23 12:17:09 +0800

    3

---
+++ prize.js
@@ -0,0 +1,15 @@
+var TEST = function(){alert('再接再厲');}
+
+        var TEST1 = function(){alert('恭喜中A獎，請上傳群組')}
+        
+        var TEST2 = function(){alert('恭喜中B獎，請上傳群組')}
+
+        var TEST3 = function(){alert('恭喜中C獎，請上傳群組')}
+
+        var TEST4 = function(){alert('恭喜中D獎，請上傳群組')}
+
+        var TEST5 = function(){alert('恭喜中E獎，請上傳群組')}
+
+        var TEST6 = function(){alert('恭喜中F獎，請上傳群組')}
+
+        var TEST7 = function(){alert('恭喜中G獎，請上傳群組')}
